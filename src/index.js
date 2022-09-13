760f71ff2c94ae13f530a8930c26a19f747aaa14
Samuel Graham <sam.graham@solertium.com>
2022-09-12 20:16:42 -0400

    Robofriends card component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='flex flex-wrap'>
+      {robots.map((robot) => (
+        <div key={robot.id}>
+          <Card 
+            id={robot.id}
+            name={robot.name}
+            username={robot.username}
+            email={robot.email}
+          />
+        </div>
+      ))}
+    </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
