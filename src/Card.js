3a9e32a726c55e2bab8a78524f2847360ea87ef7
Samuel Graham <sam.graham@solertium.com>
2022-09-25 00:57:00 -0400

    Remove tachyons and install tailwind css / updates to Card / create CardList

--- src/Card.js
+++ src/Card.js
@@ -2,14 +2,18 @@ import React from 'react'
 
 const Card = ({ id, name, username, email }) => {
   return (
-    <div id={id} className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
-      <div className="tc">
-        <h1>{username}</h1>
-        <h2>{name}</h2>
-        <p>{email}</p>
+    <a key={id} href="#" className="group">
+      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+        <img
+          src={`https://robohash.org/${id}?200x200`}
+          alt="robots"
+          className="h-full w-full object-cover object-center group-hover:opacity-75"
+        />
       </div>
-    </div>
+      <h3 className="mt-4 text-sm text-gray-700">{username}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{name}</p>
+      <p className="mt-1 text-lg font-medium text-gray-900">{email}</p>
+    </a>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
 import reportWebVitals from './reportWebVitals';
-import 'tachyons';
 import { robots } from './robots';
+import CardList from './CardList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className='flex flex-wrap'>
-      {robots.map((robot) => (
-        <div key={robot.id}>
-          <Card 
-            id={robot.id}
-            name={robot.name}
-            username={robot.username}
-            email={robot.email}
-          />
-        </div>
-      ))}
-    </div>
+    <CardList robots={robots} />
   </React.StrictMode>
 );
 
