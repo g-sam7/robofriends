b37a0d667d966fbe1abf1dbeba43c3052be67c8b
Samuel Graham <sam.graham@solertium.com>
2022-10-03 22:40:35 -0400

    Add Sega font / replace robots object with api call from json placeholder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
+import './App.css';
 import Header from "./Header";
 import Footer from "./Footer";
-import { robots } from "./robots";
 import CardList from "./CardList";
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: '',
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      return response.json();
+    })
+    .then(users => {
+      this.setState({ robots: users })
+    })
   }
 
   onSearchChange = (event) => {
